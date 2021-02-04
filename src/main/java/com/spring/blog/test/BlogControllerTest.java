97c8fdc96447307ee32c47fd59fd33c33ab9e76e
spring0913 <bbomi0913@gmail.com>
2021-02-04 23:57:37 +0900

    환경세팅 완료

---
+++ src/main/java/com/spring/blog/test/BlogControllerTest.java
@@ -0,0 +1,12 @@
+package com.spring.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>spring boot test</h1>";
+	}
+}
